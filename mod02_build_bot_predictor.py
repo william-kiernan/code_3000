72604ae9852fabd469ab037b0ffeb56d5eeea989
William Kiernan <william.kiernan@uconn.edu>
2026-02-09 21:27:30 -0500

    Tune GradientBoostingClassifier and complete discussion

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,12 +10,12 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
-        random_state=seed
-    )
+    n_estimators=300,
+    learning_rate=0.05,
+    max_depth=3,
+    subsample=0.8,
+    min_samples_leaf=5,
+    random_state=seed
+)
     model.fit(X, y)
     return model
